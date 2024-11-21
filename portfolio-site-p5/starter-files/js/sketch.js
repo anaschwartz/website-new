09e78c81c24b2d6a09ae5f3ae3d42e982c0e5d29
anaschwartz <aname4ever@gmail.com>
2024-11-21 08:53:34 -0800

    resume

--- portfolio-site-p5/starter-files/js/sketch.js
+++ portfolio-site-p5/starter-files/js/sketch.js
@@ -39,3 +39,9 @@ function drawThing(_x, _y) {
         image(gifImage, _x, _y, 250, 200); 
     }
 }
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  }
+  
